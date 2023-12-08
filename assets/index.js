eba76a4d9f52a2e8e759bd53fd0f33cd4f503dd8
ceewizz <ceewizzhuhwin@gmail.com>
2023-12-08 00:52:55 -0800

    index.js and moved the spec folder

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+// node package inquirer
+const inquirer = require("inquirer");
+
+// importing node package system file
+const fs = require("fs");
+
+// Getting the classes from directory
+const { Rectangle, Square, Triangle } = require(".spec/figures");
+
